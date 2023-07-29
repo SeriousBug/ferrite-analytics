f84227be911cccfb4856bba35c4aa1fdaf2ded9c
Kaan Barmore-Genc <kaan@bgenc.net>
2023-07-29 00:24:51 -0500

    PoC simplified version of the SQL query

--- service/src/cli/query/mod.rs
+++ service/src/cli/query/mod.rs
@@ -15,44 +15,55 @@ struct QueryData {
     filter: Filter,
 }
 
+//#[derive(Debug, Serialize, Deserialize)]
+//enum Filter {
+//    //FilterAnd(Vec<Filter>),
+//    //FilterOr(Vec<Filter>),
+//    FilterValue { name: String, eq: String },
+//}
 #[derive(Debug, Serialize, Deserialize)]
-enum Filter {
-    FilterAnd(Vec<Filter>),
-    FilterOr(Vec<Filter>),
-    FilterValue { name: String, eq: String },
+struct Filter {
+    name: String,
+    eq: String,
 }
 
 #[derive(Debug, Args)]
 pub struct QueryCommand {
     from_date: String,
     to_date: String,
+    filter: Option<String>,
 }
 
 #[async_trait::async_trait]
 impl RunCommand for QueryCommand {
     async fn run(&self) -> anyhow::Result<()> {
         let db = get_db().await?;
-        let mut events = event::Entity::find()
-            .filter(
-                event::Column::Date
-                    .gt(&self.from_date)
-                    .and(event::Column::Date.lt(&self.to_date)),
-            )
-            .filter(
-                Condition::any().add(
-                    event::Column::Key.in_subquery(
-                        Query::select()
-                            .column(property::Column::EventKey)
-                            .from(property::Entity)
-                            .and_where(Expr::col(property::Column::Name).eq("name"))
-                            .and_where(Expr::col(property::Column::Value).eq("tracking-pixel"))
-                            .to_owned(),
+
+        let mut query = event::Entity::find().filter(
+            event::Column::Date
+                .gt(&self.from_date)
+                .and(event::Column::Date.lt(&self.to_date)),
+        );
+        if let Some(filter_json) = &self.filter {
+            let filters = serde_json::from_str::<Vec<Filter>>(filter_json)?;
+
+            for filter in filters {
+                query = query.filter(
+                    Condition::any().add(
+                        event::Column::Key.in_subquery(
+                            Query::select()
+                                .column(property::Column::EventKey)
+                                .from(property::Entity)
+                                .and_where(Expr::col(property::Column::Name).eq(filter.name))
+                                .and_where(Expr::col(property::Column::Value).eq(filter.eq))
+                                .to_owned(),
+                        ),
                     ),
-                ),
-            )
-            .stream(&db)
-            .await
-            .unwrap();
+                );
+            }
+        }
+
+        let mut events = query.stream(&db).await.unwrap();
 
         println!("Events:");
         while let Some(event) = events.next().await {
