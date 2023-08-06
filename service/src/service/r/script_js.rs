581823325882d17f6cd1c95cb50ab62e1f4b9e17
Kaan Barmore-Genc <kaan@bgenc.net>
2023-08-05 23:26:58 -0500

    Fix bad comment

--- service/src/service/r/script_js.rs
+++ service/src/service/r/script_js.rs
@@ -2,7 +2,6 @@ use axum::http::header;
 use axum::response::IntoResponse;
 
 pub async fn get() -> impl IntoResponse {
-    // Respond with an 1x1 transparent png
     (
         [(header::CONTENT_TYPE, "text/javascript")],
         include_bytes!("../../../../tracker/build/out.js"),
